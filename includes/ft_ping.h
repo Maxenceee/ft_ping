fb245e33b1c906d48d01c28aef291f063258491a
Maxenceee <maxence.gama@gmail.com>
2024-07-20 19:24:19 +0200

    ping_s

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -6,13 +6,40 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 16:27:56 by mgama             #+#    #+#             */
-/*   Updated: 2024/07/16 16:28:18 by mgama            ###   ########.fr       */
+/*   Updated: 2024/07/20 19:13:43 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PING
 #define FT_PING
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
 
+#include <sys/cdefs.h>
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/uio.h>
+
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+
+#define F_VERBOSE		0x001
+#define F_COUNT			0x002
+#define F_TIMESTAMP		0x004
+
+#define F_SO_DEBUG		0x100
+#define F_SO_DONTROUTE	0x200
+
+#define PG_VERSION 1.0
 
 #endif /* FT_PING */
--- sources/ft_ping.c
+++ sources/ft_ping.c
@@ -6,13 +6,68 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 16:27:58 by mgama             #+#    #+#             */
-/*   Updated: 2024/07/16 16:28:32 by mgama            ###   ########.fr       */
+/*   Updated: 2024/07/20 19:12:39 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ping.h"
 
-int main(int argc, char const *argv[])
+int options;
+
+void	usage()
 {
+	(void)fprintf(stderr, "Usage\n");
+	(void)fprintf(stderr, "  ft_ping [options] <destination>");
+	(void)fprintf(stderr, "Options:\n");
+	(void)fprintf(stderr, "  <destination>      dns name or ip address\n");
+	(void)fprintf(stderr, "  -c <count>         stop after <count> replies\n");
+	(void)fprintf(stderr, "  -D                 print timestamps\n");
+	(void)fprintf(stderr, "  -d                 use SO_DEBUG socket option\n");
+	(void)fprintf(stderr, "  -h                 print help and exit\n");
+	(void)fprintf(stderr, "  -r                 use SO_DONTROUTE socket option\n");
+	(void)fprintf(stderr, "  -V                 print version and exit\n");
+	(void)fprintf(stderr, "  -v                 verbose output\n");
+	exit(64);
+}
+
+int main(int ac, char **av)
+{
+	char	v;
+	char	*target;
+
+	while ((v = getopt(ac, av, "c:DdhrVv")) !=  -1)
+	{
+		switch (v)
+		{
+		case 'c':
+			options |= F_COUNT;
+			break;
+		case 'D':
+			options |= F_TIMESTAMP;
+			break;
+		case 'd':
+			options |= F_SO_DEBUG;
+			break;
+		case 'r':
+			options |= F_SO_DONTROUTE;
+			break;
+		case 'V':
+			printf("ft_ping v%.1f\n", PG_VERSION);
+			exit(0);
+			break;
+		case 'v':
+			options |= F_VERBOSE;
+			break;
+		default:
+			usage(av);
+		}
+	}
+
+	if (ac - optind != 1)
+		usage();
+	target = av[optind];
+
+	printf("target: %s\n", target);
+
 	return (0);
 }
